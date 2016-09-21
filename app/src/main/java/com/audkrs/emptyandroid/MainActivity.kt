4b0a18bcdc0c31d754c242bb39dd6e2880f1a2fc
Audrius <audrius.karosevicius@gmail.com>
2016-09-21 12:36:26 +0300

    Some amazing changes

--- app/src/main/java/com/audkrs/emptyandroid/MainActivity.kt
+++ app/src/main/java/com/audkrs/emptyandroid/MainActivity.kt
@@ -2,11 +2,16 @@ package com.audkrs.emptyandroid
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Hello world!", Toast.LENGTH_SHORT).show()
+        text_view.setOnClickListener(View.OnClickListener { /*nothing here*/ });
     }
 }
