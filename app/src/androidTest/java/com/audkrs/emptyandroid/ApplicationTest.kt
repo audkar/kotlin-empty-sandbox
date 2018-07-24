5052045a79a710b16e49111197aa4b6999007f0e
Audrius Karosevicius <audrius.karosevicius@gmail.com>
2018-07-24 17:58:21 +0300

    Update dependencies

---
+++ app/src/androidTest/java/com/audkrs/emptyandroid/ApplicationTest.kt
@@ -0,0 +1,6 @@
+package com.audkrs.emptyandroid
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
