9c7e7ded63fe225e6710982e313240289e385adf
Audrius Karosevicius <audrius.karosevicius@gmail.com>
2018-10-16 10:30:12 +0300

    Migrate to AndroidX

--- app/src/main/java/com/audkrs/emptyandroid/MainActivity.kt
+++ app/src/main/java/com/audkrs/emptyandroid/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.audkrs.emptyandroid
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
