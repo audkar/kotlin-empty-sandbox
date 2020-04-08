8c20e93bc905c1ab02b5b55c3887e8407bd7ee57
Audrius Karosevicius <audrius.karosevicius@gmail.com>
2020-04-08 10:49:42 +0300

    Migrate to Gradle Kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("kotlin-android-extensions")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdkVersion(29)
+    defaultConfig {
+        applicationId = "com.audkrs.emptyandroid"
+        minSdkVersion(21)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+    testOptions {
+        unitTests.isIncludeAndroidResources = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(project(":main"))
+    implementation(kotlin("stdlib"))
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.3")
+    implementation("androidx.annotation:annotation:1.1.0")
+    implementation("com.google.android.material:material:1.1.0")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("androidx.core:core-ktx:1.2.0")
+    implementation("com.google.dagger:dagger-android-support:${Versions.dagger}")
+    kapt("com.google.dagger:dagger-compiler:${Versions.dagger}")
+    kapt("com.google.dagger:dagger-android-processor:${Versions.dagger}")
+
+    testImplementation("androidx.test:core:1.2.0")
+    testImplementation("androidx.test:runner:1.2.0")
+    testImplementation("androidx.test:rules:1.2.0")
+    testImplementation("androidx.test.ext:junit:1.1.1")
+    testImplementation("androidx.test.ext:truth:1.2.0")
+    testImplementation("com.google.truth:truth:1.0.1")
+    testImplementation("androidx.test.espresso:espresso-core:3.2.0")
+    testImplementation("org.robolectric:robolectric:4.3.1")
+
+    androidTestImplementation("androidx.test:core:1.2.0")
+    androidTestImplementation("androidx.test:runner:1.2.0")
+    androidTestImplementation("androidx.test:rules:1.2.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.ext:truth:1.2.0")
+    androidTestImplementation("com.google.truth:truth:1.0.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+}
+
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,8 @@
+object Versions {
+    const val kotlin = "1.3.71"
+    const val agp = "3.6.2"
+    const val coroutines = "1.3.5"
+    const val ktor = "1.3.2"
+    const val serialization = "0.20.0"
+    const val dagger = "2.27"
+}
