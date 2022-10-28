5e2f63fb412b91d1137d4330837dcd4e8db42ab4
Audrius Karosevicius <audrius.karosevicius@gmail.com>
2022-10-28 17:15:56 +0300

    AGP 7.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("com.android.tools.build:gradle:7.3.1")
         classpath(kotlin("gradle-plugin", "1.7.20"))
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
     }
