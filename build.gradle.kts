32242d0dd411c1ae337e07fc9ab6a4c42e3d4299
Audrius Karosevicius <audrius.karosevicius@gmail.com>
2022-12-27 14:16:52 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.1")
-        classpath(kotlin("gradle-plugin", "1.7.20"))
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
+        classpath(kotlin("gradle-plugin", "1.7.22"))
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
     }
 }
 
