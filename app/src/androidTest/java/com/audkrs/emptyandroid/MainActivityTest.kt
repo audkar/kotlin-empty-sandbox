94488c22fccdbea1dac69d26bc88e173b9068205
Audrius Karosevicius <audrius.karosevicius@gmail.com>
2023-02-15 12:07:39 +0200

    Fix tests

--- app/src/androidTest/java/com/audkrs/emptyandroid/MainActivityTest.kt
+++ app/src/androidTest/java/com/audkrs/emptyandroid/MainActivityTest.kt
@@ -1,13 +1,8 @@
 package com.audkrs.emptyandroid
 
-import androidx.lifecycle.Lifecycle
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import com.audkrs.emptyandroid.databinding.ActivityMainBinding
-import com.google.common.truth.Truth.assertThat
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
@@ -19,21 +14,15 @@ class MainActivityTest {
     @Rule(order = 0)
     @JvmField
     val hiltRule = HiltAndroidRule(this)
+
     @Rule(order = 1)
     @JvmField
-    val scenarioRule = ActivityScenarioRule(MainActivity::class.java)
+    val scenarioRule = createAndroidComposeRule<MainActivity>()
 
     @Test
     fun name() {
-        val scenario = scenarioRule.scenario
-        scenario.moveToState(Lifecycle.State.RESUMED)
-        onView(withText("Hello World!")).check(
-            matches(isCompletelyDisplayed())
-        )
-
-        scenario.onActivity {
-            val binding = ActivityMainBinding.bind(it.requireViewById(R.id.activity_content))
-            assertThat(binding.helloText.text).isEqualTo("Hello World!")
+        with(scenarioRule) {
+            onNode(hasText("Hello world!")).assertIsDisplayed()
         }
     }
 }
--- app/src/test/java/com/audkrs/emptyandroid/MainActivityTest.kt
+++ app/src/test/java/com/audkrs/emptyandroid/MainActivityTest.kt
@@ -1,14 +1,11 @@
 package com.audkrs.emptyandroid
 
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.lifecycle.Lifecycle
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.audkrs.emptyandroid.databinding.ActivityMainBinding
-import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
@@ -24,20 +21,17 @@ class MainActivityUnitTest {
     val rule = HiltAndroidRule(this)
     @Rule(order = 1)
     @JvmField
-    val scenarioRule = ActivityScenarioRule(MainActivity::class.java)
+    val scenarioRule = createAndroidComposeRule<MainActivity>()
 
     @Test
     fun name() {
-        val scenario = scenarioRule.scenario
+        val scenario = scenarioRule.activityRule.scenario
         scenario.moveToState(Lifecycle.State.RESUMED)
 
-        scenario.onActivity {
-            val binding = ActivityMainBinding.bind(it.requireViewById(R.id.activity_content))
-            assertThat(binding.helloText.text).isEqualTo("Hello World!")
-            binding.clickMe.performClick()
+        with(scenarioRule){
+            onNodeWithText("Hello world!").assertIsDisplayed()
+            onNodeWithText("click me").performClick()
+            onNodeWithText("BAZINGA").assertIsDisplayed()
         }
-
-        onView(withId(R.id.hello_text))
-            .check(matches(withText("BAZINGA")))
     }
 }
