91a13f515f0a1ad75f5591199d8c4ed0bf706c53
Audrius Karosevicius <audrius.karosevicius@gmail.com>
2023-06-19 15:39:54 +0300

    Cache debug

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,14 @@
 rootProject.name = "kotlin-empty-sandbox"
 
+plugins {
+    id("com.gradle.enterprise") version("3.13.3")
+}
+
 include(":app", ":main")
+
+gradleEnterprise {
+  buildScan {
+      termsOfServiceUrl = "https://gradle.com/terms-of-service"
+      termsOfServiceAgree = "yes"
+  }
+}
