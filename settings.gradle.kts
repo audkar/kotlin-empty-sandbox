bf493e1cd9346205fedd12ea5776f206bf45b448
Audrius Karosevicius <audrius.karosevicius@gmail.com>
2023-08-30 22:57:52 +0300

    Update gradle.enterprise plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "kotlin-empty-sandbox"
 
 plugins {
-    id("com.gradle.enterprise") version("3.13.3")
+    id("com.gradle.enterprise") version("3.14.1")
 }
 
 include(":app", ":main")
