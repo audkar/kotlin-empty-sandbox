06714851879b5562dadf3e43337562496693574d
Audrius Karosevicius <audrius.karosevicius@gmail.com>
2023-09-21 19:06:56 +0300

    Deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,14 +48,14 @@ kotlin {
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2023.08.00")
+    val composeBom = platform("androidx.compose:compose-bom:2023.09.01")
 
     implementation(project(":main"))
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("androidx.annotation:annotation:1.6.0")
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.annotation:annotation:1.7.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-ktx:1.7.2")
     implementation("com.google.dagger:hilt-android:2.48")
     ksp("com.google.dagger:hilt-android-compiler:2.48")
@@ -68,7 +68,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.compose.material:material-icons-core")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
     lintChecks("com.slack.lint.compose:compose-lint-checks:1.2.0")
 
--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -33,13 +33,13 @@ kotlin {
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2023.08.00")
+    val composeBom = platform("androidx.compose:compose-bom:2023.09.01")
 
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("androidx.annotation:annotation:1.6.0")
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.annotation:annotation:1.7.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("com.google.dagger:hilt-android:2.48")
     ksp("com.google.dagger:hilt-android-compiler:2.48")
 
@@ -50,5 +50,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.compose.material:material-icons-core")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 }
