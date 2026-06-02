af5c2b63cae6ebebc7df53bf51a0892bd77cb040
Audrius Karosevicius <audrius.karosevicius@gmail.com>
2026-06-02 13:26:56 +0300

    Refactor deprecated androidx test method

--- app/src/test/java/com/audkrs/emptyandroid/MainActivityTest.kt
+++ app/src/test/java/com/audkrs/emptyandroid/MainActivityTest.kt
@@ -1,7 +1,7 @@
 package com.audkrs.emptyandroid
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.lifecycle.Lifecycle
